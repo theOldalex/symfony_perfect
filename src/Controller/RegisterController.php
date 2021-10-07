be38a55d23e112dc355d49492992edcd62092955
theOldalex <alexandrediakitepro@gmail.com>
2021-10-07 16:59:32 +0200

    maj

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use App\Controller\UtilisateurController;
+use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,13 +15,22 @@ class RegisterController extends AbstractController
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         
         $utilisateur = new Utilisateur;
+        $form = $this->createForm(RegisterType::class, $utilisateur) ;
         
-        
-        
+        $form ->handleRequest($request) ;
+
+         (if $form->isSubmitted() && if $form->isValid()){
+
+            $utilisateur = $form->getData();
+            
+
+        }
+
+         
         
         return $this->render('register/index.html.twig');
     }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,21 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('motDePasse')
-            ->add('pseudo')
             
+            ->add('pseudo', TextType::class,[
+                "label"=>false,
+                "required"=>true
+            ])
+            ->add('motDePasse', PasswordType::class,[
+                "label"=>false,
+                "required"=>true
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S/inscrire'
+            ]);
+
         ;
+        
     }
 
     public function configureOptions(OptionsResolver $resolver): void
